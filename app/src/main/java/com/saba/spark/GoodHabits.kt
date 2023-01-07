b4c386eabe3f4211c782f2362ff8ba3cfeb93e57
NutsaJ <102469713+NutsaJ@users.noreply.github.com>
2023-01-08 02:50:39 +0400

    guka changebi ra smn

--- app/src/main/java/com/saba/spark/GoodHabits.kt
+++ app/src/main/java/com/saba/spark/GoodHabits.kt
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import com.saba.spark.databinding.FragmentBadHabitsBinding
 import com.saba.spark.databinding.FragmentGoodHabitsBinding
 
@@ -17,21 +19,43 @@ class GoodHabits : Fragment(R.layout.fragment_good_habits) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentGoodHabitsBinding.bind(view)
+        val editSpentMoney = binding.editSpentMoney
+        val moneySpentPerDay = binding.moneySpentPerDay
 
-        showEditTextDialog()
+
+
+
+        editSpentMoney.setOnClickListener{
+
+            val mDialogView = LayoutInflater.from(context).inflate(R.layout.good_habits_edit_money , null);
+            val mBuilder = AlertDialog.Builder(context)
+                .setView(mDialogView)
+                .setTitle("Bad Habit")
+            val mAlertDialog = mBuilder.show()
+            var submit =mAlertDialog.findViewById<Button>(R.id.submit)
+            var cancel = mAlertDialog.findViewById<TextView>(R.id.cancelButton)
+
+            submit.setOnClickListener{
+                mAlertDialog.dismiss()
+                var editmoney1 = mAlertDialog.findViewById<EditText>(R.id.editMoney)
+                var editMoney = editmoney1.text.toString()
+                moneySpentPerDay.setText(editMoney + " Gel")
+
+            }
+            cancel.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+
 
         }
 
-    private fun showEditTextDialog() {
-        val editMoney = binding.editSpentMoney
-        editMoney.setOnClickListener{
-            val builder = AlertDialog.Builder(context)
-            val inflater = LayoutInflater(context)
-            val dialogLayout = inflater.inflate(R.layout.good_habits_settings , null)
-            val editText = dialogLayout.findViewById<EditText>(R.id.)
-        }
+
 
     }
+
+
+
+
 
 
 }
